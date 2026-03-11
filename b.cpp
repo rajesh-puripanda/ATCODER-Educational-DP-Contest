8e2b2183ca51db283ad32c385551031fb50b5864
RajeshPuripanda <rajesh.puripanda@icloud.com>
2026-03-12 01:22:29 +0530

    B - Frog 2

---
+++ b.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int n,k;
+	cin >> n >> k;
+	
+	vector<int> h(n);
+	for(int &x: h) cin >> x;
+	
+	vector<int> dp(n, INT_MAX);
+	dp[0] = 0;
+	
+	for (int i=1; i<n; ++i){
+			for(int j=1; j <=k; ++j){
+				if (i-j>=0){
+					dp[i] = min(dp[i], dp[i-j]+abs(h[i]-h[i-j]));
+				}
+			}
+	}
+	
+	cout << dp[n-1] << endl;
+	
+	return 0;
+	
+}
+
